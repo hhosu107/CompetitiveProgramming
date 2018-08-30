f09a0af2913cac7da7ffde5598aedf4266747084
Chansu Park <hhosu107@gmail.com>
2018-08-30 18:30:00 +0900

    Text echo

---
+++ Baekjoon/11718.cpp
@@ -0,0 +1,11 @@
+#include<string>
+#include<algorithm>
+#include<functional>
+#include<cctype>
+#include<iostream>
+using namespace std;
+int main(){
+  string s;
+  while(getline(cin, s)){cout << s << endl;}
+  return 0;
+}
