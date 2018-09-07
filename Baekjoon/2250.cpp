8fd8f7f5d352fd5f595faade3e206767a2ef252c
Chansu Park <hhosu107@gmail.com>
2018-09-07 16:59:00 +0900

    Need to edit 2250

--- Baekjoon/2250.cpp
+++ Baekjoon/2250.cpp
@@ -17,6 +17,8 @@ using namespace std;
 int parent[10001]; // contains parent
 int link[10001][2]; // contains left/right index of the node
 int childs[10001][3]; // 0th: height, 1st: # of left childs, 2nd: # of right childs
+int distIdx[10001]; // containing distance from the center
+int widthIdx[10001][2]; // 0th: contains leftmost distance from the root of the level, 1th: contains rightmost distance from the root of the level
 
 void settle(int n){
   if(parent[n] != -1){
@@ -33,6 +35,32 @@ void settle(int n){
   return;
 }
 
+void distance(int node, int direction, int isLeft){
+  // level = childs[node][0], direction = left child (1) of the parent or right child (2) of the parent,
+  // isleft = 1 if finding leftmost, 2 if finding rightmost
+  if(node == -1) return; // Just filter the non-existing case by this.
+  distIdx[node] = 0;
+  // Insert the absolute distance from the root.
+  if(direction != 0){
+    // distance between the direct parent is defined by the other side of childs.
+    distIdx[node] = (childs[node][3 - direction] + 1) * (direction == isLeft ? 1 : -1) + distIdx[parent[node]];
+  }
+  // Update the maximum absolute distance from the root.
+  if(isLeft != 0){
+    if(widthIdx[childs[node][0]][isLeft-1] < distIdx[node]){
+      widthIdx[childs[node][0]][isLeft-1] = distIdx[node];
+    }
+  }
+  // Go left with initial direction = 1, isLeft = 1 and right with direction = 2, isLeft = 2 if it is the root
+  if(isLeft == 0){
+    distance(link[node][0], 1, 1);
+    distance(link[node][1], 2, 2);
+  }
+  else{
+    distance(link[node][0], 1, isLeft);
+    distance(link[node][1], 2, isLeft);
+  }
+}
 int main(){
   int N, index;
   cin >> N;
@@ -59,5 +87,14 @@ int main(){
   }
   childs[root][0] = 1;
   settle(root);
+  distance(root, 0, 0);
+  int rep = 0, width = 0;
+  for(int i=1; i<=10000; i++){
+    if(width < widthIdx[i][0] + widthIdx[i][1] + 1){
+      width = widthIdx[i][0] + widthIdx[i][1] + 1;
+      rep = i;
+    }
+  }
+  printf("%d %d\n", rep, width);
   return 0;
 }
