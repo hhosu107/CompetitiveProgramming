5cd1838aeb9089dfeab2f476627a54bae2bc3b53
Chansu Park <hhosu107@gmail.com>
2020-04-03 23:22:57 +0900

    2020 GCJ

---
+++ CodeJam/2020/Q/a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  return 0;
+}
