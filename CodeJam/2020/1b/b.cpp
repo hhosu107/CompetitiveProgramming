c6e8f24f23f027aa720363894834afa499c0aaf5
Chansu Park <hhosu107@gmail.com>
2020-04-20 00:23:27 +0900

    2020 1B prepare

---
+++ CodeJam/2020/1b/b.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  int T;
+  cin >> T;
+  for (int t=1; t<=T; t++){
+    cout << "Case #" << t << ": ";
+  }
+  return 0;
+}
