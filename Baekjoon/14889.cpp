fecbc1e50bc3406e31c71d74a74f2f1077557c99
Chansu Park <hhosu107@gmail.com>
2021-06-29 00:56:24 +0900

    14889

--- Baekjoon/14889.cpp
+++ Baekjoon/14889.cpp
@@ -3,12 +3,11 @@
 #include<algorithm>
 using namespace std;
 
-int statdiff(vector<vector<int>> &crossstat, int n, vector<int> &lefts, vector<int> &rights,
-    int sumleft, int sumright, int pleft, int pright){
+int statdiff(vector<vector<int>> crossstat, int n, vector<int> &lefts, vector<int> &rights, int sumleft, int sumright, int pleft, int pright){
   if (pleft == (n / 2) && pright == (n / 2)){
     return (sumleft > sumright ? sumleft - sumright : sumright - sumleft);
   }
-  int statdiff = 2147483647;
+  int mindiff = 2147483647;
   if (pleft < (n / 2)){
     int currleft = sumleft;
     for(int i=0; i<pleft; i++){
@@ -16,21 +15,32 @@ int statdiff(vector<vector<int>> &crossstat, int n, vector<int> &lefts, vector<i
     }
     lefts[pleft] = pleft + pright;
     int currdiff = statdiff(crossstat, n, lefts, rights, currleft, sumright, pleft + 1, pright);
-    if (statdiff > currdiff) statdiff = currdiff;
+    if (mindiff > currdiff) mindiff = currdiff;
   }
   if (pright < (n / 2)){
     int currright = sumright;
     for(int i=0; i<pright; i++){
       currright += (crossstat[rights[i]][pleft + pright] + crossstat[pleft + pright][rights[i]]);
     }
-    rights[pleft] = pleft + pright;
+    rights[pright] = pleft + pright;
     int currdiff = statdiff(crossstat, n, lefts, rights, sumleft, currright, pleft, pright + 1);
-    if (statdiff > currdiff) statdiff = currdiff;
+    if (mindiff > currdiff) mindiff = currdiff;
   }
 
-  return 0;
+  return mindiff;
 }
 
 int main(){
+  int n;
+  cin >> n;
+  vector<vector<int>> crossstat = vector<vector<int>>(n, vector<int>(n, 0));
+  vector<int> lefts = vector<int>(n/2, 0);
+  vector<int> rights = vector<int>(n/2, 0);
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++)
+    cin >> crossstat[i][j];
+  }
+  int mindiff = statdiff(crossstat, n, lefts, rights, 0, 0, 0, 0);
+  cout << mindiff << endl;
   return 0;
 }
