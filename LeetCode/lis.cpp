d96f2b8224f36e7b132635d11755f3136029540f
Chansu Park <hhosu107@gmail.com>
2022-01-28 14:16:02 +0900

    LIS

---
+++ LeetCode/lis.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int longestIncreasingSubsequence(vector<int> &x){
+    int n = x.size();
+    vector<int> dp(n, 1);
+    int maxInc = 0;
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < i; j++){
+            if(x[i] > x[j]){
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        maxInc = max(maxInc, dp[i]);
+    }
+    return maxInc;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    for(int i = 0; i < n; i++){
+        cin >> x[i];
+    }
+    cout << longestIncreasingSubsequence(x) << endl;
+    return 0;
+}
