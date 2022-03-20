b4dc0c29afba6be16af318d4ca48609c9e898f62
Chansu Park <hhosu107@gmail.com>
2022-03-20 16:36:44 +0900

    20220320

---
+++ Baekjoon/1912.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n), subsum(n);
+  int best = 0;
+  int curr = 0;
+  bool all_neg = true;
+  int neg_best = -999999;
+  for(int i=0; i<n; i++) {
+    cin >> a[i];
+    curr = max(0, curr + a[i]);
+    best = max(best, curr);
+    if (a[i] >= 0) {
+      all_neg = false;
+    } else {
+      neg_best = max(neg_best, a[i]);
+    }
+  }
+  cout << (all_neg ? neg_best : best) << '\n';
+  return 0;
+}
