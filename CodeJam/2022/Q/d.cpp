0fcaff3ef834b6403b10c629b066d385656a033f
Chansu Park <hhosu107@gmail.com>
2022-04-02 23:40:53 +0900

    20220402 GCJ Qual D-small

--- CodeJam/2022/Q/d.cpp
+++ CodeJam/2022/Q/d.cpp
@@ -18,10 +18,54 @@ using namespace std;
 // And then 4 activates, which gets max(50, 20) = 50.
 // This is the maximum.
 // Given the fun factors and the setup, compute the maximum fun.
+using ll = long long;
 int main () {
   int T, N;
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
+  // D small: make a permutation on initiators; do all and find the max.
+  cin >> T;
+  for(int t=1; t<=T; t++) {
+    cin >> N;
+    vector<ll> fun(N + 1);
+    map<int, int> m;
+    m[0] = 0;
+    for(int i=1; i<=N; i++) {
+      cin >> fun[i];
+    }
+    vector<bool> initable(N + 1, true);
+    initable[0] = false;
+    for(int i=1; i<=N; i++) {
+      int j;
+      cin >> j;
+      initable[j] = false;
+      m[i] = j;
+    }
+    vector<int> initiators;
+    for(int i=1; i<=N; i++) {
+      if(initable[i]) {
+        initiators.push_back(i);
+      }
+    }
+    ll max_fun = 0;
+    vector<bool> visited;
+    do {
+      ll sum_fun = 0;
+      visited = vector<bool>(N + 1, false);
+      for(int i=0; i<initiators.size(); i++) {
+        ll cur_fun = 0;
+        ll cur = initiators[i];
+        while(!visited[cur]) {
+          visited[cur] = true;
+          cur_fun = max(cur_fun, fun[cur]);
+          cur = m[cur];
+        }
+        sum_fun += cur_fun;
+      }
+      max_fun = max(max_fun, sum_fun);
+    }while(next_permutation(initiators.begin(), initiators.end()));
+    cout << "Case #" << t << ": " << max_fun << '\n';
+  }
   return 0;
 }
