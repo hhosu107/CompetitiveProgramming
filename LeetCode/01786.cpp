ba59bf66cd57bcc6e8027cbb7614e297e5d3f6a7
Chansu Park <hhosu107@gmail.com>
2022-04-04 14:48:47 +0900

    Leetcode 20220404

---
+++ LeetCode/01786.cpp
@@ -0,0 +1,61 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#include<map>
+#include<queue>
+using namespace std;
+
+/* Count number of fastest paths from source to destination.
+ * Idea: If we find a shorter path (s...i-d), update count[d] = count[i].
+ * If we find a path with the same length (s...i-d), update count[d] + count[i].
+ * finally return count[D].
+ * Leetcode 1976.
+ * Reference: https://www.baeldung.com/cs/graph-number-of-shortest-paths
+ */
+using pii = pair<int, int>;;
+using ll = long long;
+int countPaths(int n, vector<vector<int>> &roads) {
+    int s = 0, D = n - 1;
+    map<int, vector<pii>> adj;
+    for (auto road: roads) {
+        if (adj.find(road[0]) == adj.end()) adj[road[0]] = vector<pii>();
+        if (adj.find(road[1]) == adj.end()) adj[road[1]] = vector<pii>();
+        adj[road[0]].push_back({road[1], road[2]});
+        adj[road[1]].push_back({road[0], road[2]});
+    }
+    vector<int> distance(n, INT_MAX);
+    vector<ll> paths(n, 0);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    pq.push({0, s});
+    distance[s] = 0;
+    paths[s] = 1;
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if (adj.find(u) == adj.end()) continue;
+        for (auto [v, w]: adj[u]) {
+            if (distance[v] > d + w) {
+                pq.push({d + w, v});
+                distance[v] = d + w;
+                paths[v] = paths[u];
+            } else if (distance[v] == d + w) {
+                paths[v] = (paths[v] + paths[u]) % 1000000007;
+            }
+        }
+    }
+    return paths[D];
+}
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> roads(m, vector<int>(3));
+    for(auto &road: roads)
+        cin >> road[0] >> road[1] >> road[2];
+    int num_paths = countPaths(n, roads);
+    cout << num_paths << '\n';
+    return 0;
+}
---
+++ LeetCode/02101.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+using ll = long long;
+
+int maximumDetonation(vector<vector<int>>& bombs) {
+  vector<vector<int>> directed_bombs(bombs.size(), vector<int>());
+  vector<ll> rad_sq(bombs.size());
+  for (int i=0; i<bombs.size(); i++)
+    rad_sq[i] = (ll)bombs[i][2] * (ll)bombs[i][2];
+  for (int i=0; i<bombs.size(); i++) {
+    for(int j=0; j<bombs.size(); j++) {
+      if (i == j) continue;
+      if (rad_sq[i] >= (ll)(bombs[i][0] - bombs[j][0]) * (ll)(bombs[i][0] - bombs[j][0]) + (ll)(bombs[i][1] - bombs[j][1]) * (ll)(bombs[i][1] - bombs[j][1]))
+        directed_bombs[i].push_back(j);
+    }
+  }
+  queue<int> reachable_bombs;
+  vector<bool> reached;
+  int max_bombs = 0;
+  for(int i=0; i<bombs.size(); i++) {
+    reachable_bombs.push(i);
+    reached = vector<bool>(bombs.size(), false);
+    reached[i] = true;
+    int bomb_cnt = 1;
+    while(!reachable_bombs.empty()) {
+      int cur = reachable_bombs.front();
+      reachable_bombs.pop();
+      for (auto next_bomb: directed_bombs[cur]) {
+        if (reached[next_bomb]) continue;
+        reachable_bombs.push(next_bomb);
+        reached[next_bomb] = true;
+        bomb_cnt++;
+      }
+    }
+    max_bombs = max(max_bombs, bomb_cnt);
+  }
+  return max_bombs;
+}
