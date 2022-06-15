48a6a4e4dcbca44bdff2775ff4fff8f647be2f94
Chansu Park <hhosu107@gmail.com>
2022-06-16 01:38:46 +0900

    Leetcode 20220615 Hot...

---
+++ LeetCode/00801.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minSwap(vector<int>& A, vector<int>& B) {
+        const size_t n = A.size();
+        vector<int> swap(n, n), no_swap(n, n);
+        swap[0] = 1;
+        no_swap[0] = 0;
+        for (size_t i = 1; i < n; ++i) {
+            if (A[i] > A[i - 1] && B[i] > B[i - 1]) {
+                // If we swap position i, since we reversed the condition above,
+                // we need to swap position i - 1.
+                swap[i] = swap[i - 1] + 1;
+                
+                // If we don't swap position i ,
+                // since we follow the condition above,
+                // we should not swap position i - 1.
+                no_swap[i] = no_swap[i - 1];
+            }
+            
+            if (A[i] > B[i - 1] && B[i] > A[i - 1]) {
+                // If we swap position i,
+                // since we follow the condition above,
+                // we should not swap position i - 1.
+                swap[i] = std::min(swap[i], no_swap[i - 1] + 1);
+                
+                // If we don't swap position i,
+                // to follow the condition above,
+                // we should swap position i - 1.
+                no_swap[i] = std::min(no_swap[i], swap[i - 1]);
+            }
+        }
+        
+        return std::min(swap.back(), no_swap.back());
+    }
+};
